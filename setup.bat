80b8f47e58c929a8b56cefb738db2f63ed15824c
Christoph Honal <christoph.honal@web.de>
2020-12-05 03:12:23 +0100

    setp

--- setup.bat
+++ setup.bat
@@ -2,38 +2,38 @@
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 
-@REM mkdir protobuf 2> NUL
-@REM cd protobuf
-@REM cmake -G "Visual Studio 16 2019" -A Win32 -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DCMAKE_BUILD_TYPE=Release --config Release -Dprotobuf_BUILD_PROTOC_BINARIES=OFF -Dprotobuf_WITH_ZLIB=OFF ..\fivem\vendor\protobuf\cmake
-@REM cd ..
+mkdir protobuf 2> NUL
+cd protobuf
+cmake -G "Visual Studio 16 2019" -A Win32 -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DCMAKE_BUILD_TYPE=Release --config Release -Dprotobuf_BUILD_PROTOC_BINARIES=OFF -Dprotobuf_WITH_ZLIB=OFF ..\fivem\vendor\protobuf\cmake
+cd ..
 
-@REM cd fivem\vendor\libuv
-@REM git apply ..\..\..\patch\libuv.patch
-@REM cd ..\..\..\
-@REM mkdir libuv 2> NUL
-@REM cd libuv
-@REM cmake -G "Visual Studio 16 2019" -A Win32 -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DCMAKE_BUILD_TYPE=Release --config Release -DBUILD_TESTING=OFF ..\fivem\vendor\libuv
-@REM cd ..
+cd fivem\vendor\libuv
+git apply ..\..\..\patch\libuv.patch
+cd ..\..\..\
+mkdir libuv 2> NUL
+cd libuv
+cmake -G "Visual Studio 16 2019" -A Win32 -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DCMAKE_BUILD_TYPE=Release --config Release -DBUILD_TESTING=OFF ..\fivem\vendor\libuv
+cd ..
 
-@REM cd fivem\vendor\labsound
-@REM git apply ..\..\..\patch\labsound.patch
-@REM cd ..\..\..\
-@REM mkdir labsound 2> NUL
-@REM cd labsound
-@REM cmake -G "Visual Studio 16 2019" -A Win32 -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DCMAKE_BUILD_TYPE=Release CXXFLAGS --config Release ..\fivem\vendor\labsound
-@REM cd ..
+cd fivem\vendor\labsound
+git apply ..\..\..\patch\labsound.patch
+cd ..\..\..\
+mkdir labsound 2> NUL
+cd labsound
+cmake -G "Visual Studio 16 2019" -A Win32 -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DCMAKE_BUILD_TYPE=Release CXXFLAGS --config Release ..\fivem\vendor\labsound
+cd ..
 
-@REM cd fivem\vendor\fmtlib
-@REM git apply ..\..\..\patch\fmtlib.patch
-@REM cd ..\..\..\
-@REM mkdir fmtlib 2> NUL
-@REM cd fmtlib
-@REM cmake -G "Visual Studio 16 2019" -A Win32 -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DCMAKE_BUILD_TYPE=Release --config Release ..\fivem\vendor\fmtlib
-@REM cd ..
+cd fivem\vendor\fmtlib
+git apply ..\..\..\patch\fmtlib.patch
+cd ..\..\..\
+mkdir fmtlib 2> NUL
+cd fmtlib
+cmake -G "Visual Studio 16 2019" -A Win32 -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DCMAKE_BUILD_TYPE=Release --config Release ..\fivem\vendor\fmtlib
+cd ..
 
-@REM cd botan\botan
-@REM python .\configure.py --cc=msvc --os=windows --define-build-macro=_DISABLE_EXTENDED_ALIGNED_STORAGE --cpu=x86_32 --msvc-runtime=MT --disable-shared-library --prefix=.
-@REM cd ..\..
+cd botan\botan
+python .\configure.py --cc=msvc --os=windows --define-build-macro=_DISABLE_EXTENDED_ALIGNED_STORAGE --cpu=x86_32 --msvc-runtime=MT --disable-shared-library --prefix=.
+cd ..\..
 
 cd ffmpeg
 git apply ..\patch\ffmpeg.patch
